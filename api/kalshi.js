730b914e744ac2f53aab5eb2c464099e8cbe1143
Pchow889 <jlicari042@gmail.com>
2026-04-29 11:43:33 -0700

    Add API handler for Kalshi election data

---
+++ api/kalshi.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const url = req.query.url;
+  if (!url || !url.startsWith('https://api.elections.kalshi.com')) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
+  const response = await fetch(url, {
+    headers: { 'Accept': 'application/json' }
+  });
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(response.status).json(data);
+}
